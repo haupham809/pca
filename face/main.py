3573b0530dacb4ecd072846ebdcc9e65fd5e195e
haupham809 <56516439+haupham809@users.noreply.github.com>
2020-10-27 16:10:10 +0700

    Update main.py

--- face/main.py
+++ face/main.py
@@ -3,14 +3,19 @@ import face_recognition
 import numpy as np
 import os
 import time
+import  shutil
 
 while True:
 
         path = 'anh'
         tenanh = []
         classname = []
-        listfoder = os.listdir(path)
+        listfoder1 = os.listdir(path)
         slanh = []
+        if ("facenew" in listfoder1):
+            shutil.rmtree('anh/facenew')
+        listfoder = os.listdir(path)
+        print(listfoder)
         for s in listfoder:
             listimage = os.listdir(f'{path}/{s}')
             slanh.append(len(listimage))
@@ -18,13 +23,16 @@ while True:
                 ci = cv2.imread(f'{path}/{s}/{i}')
                 tenanh.append(ci)
                 classname.append(os.path.splitext(i)[0])
+            print(listimage)
         def findencode(images):
             encodelist = []
             for j in images:
                 j = cv2.cvtColor(j, cv2.COLOR_BGR2RGB)
                 encode = face_recognition.face_encodings(j)[0]
+                print(encode)
                 encodelist.append(encode)
             return encodelist
+
         encodelistknown = findencode(tenanh)
         def namefolder(lc):
             for k in range(0, len(listfoder)):
@@ -41,28 +49,29 @@ while True:
                 for encodeface, faceloc in zip(encodeframe, faceframe):
                     matches = face_recognition.compare_faces(encodelistknown, encodeface)
                     facedis = face_recognition.face_distance(encodelistknown, encodeface)
+                    print(facedis)
                     matchindex = np.argmin(facedis)
                     y1, x2, y2, x1 = faceloc
                     y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
-                    cv2.rectangle(im, (x1, y1), (x2, y2), (255, 255, 255), 2)
                     if matches[matchindex]:
+                        cv2.rectangle(im, (x1, y1), (x2, y2), (255, 255, 255), 2)
                         cv2.putText(im, namefolder(matchindex), (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 255), 2)
                     else:
-                        anh1 = cv2.getRectSubPix(im, (x2 - x1, y2 - y1), ((x2 + x1) / 2, (y1 + y2) / 2))
-                        cv2.imshow('webcam', anh1)
+                        anh = cv2.getRectSubPix(im, (x2 - x1, y2 - y1), ((x2+ x1) / 2, (y1+ y2) / 2))
+                        cv2.imshow('webcam', anh)
                         cv2.waitKey(1)
+                        path1 = os.path.join(os.getcwd(), 'anh', "facenew")
+                        os.mkdir(path1)
+                        for ix in range(0, 10):
+                            cv2.waitKey(10)
+                            anh = cv2.getRectSubPix(im, (x2 - x1, y2 - y1), ((x2 + x1) / 2, (y1 + y2) / 2))
+                            cv2.imwrite(path1 + '/' + "anh" + str(int(time.time()))+str(ix) + str(ix) + ".jpg", anh)
+
                         x = input("nhap ten  cua ban:")
                         if x in listfoder:
                             print("ten ban da ton tai ")
                         else:
-                            nh = cv2.getRectSubPix(im, (x2 - x1, y2 - y1), ((x2 + x1) / 2, (y1 + y2) / 2))
-                            cv2.imshow('webcam', nh)
-                            path1 = os.path.join(os.getcwd(), 'anh', x)
-                            os.mkdir(path1)
-                            for ix in range(0, 10):
-                                anh = cv2.getRectSubPix(im, (x2 - x1, y2 - y1), ((x2 + x1) / 2, (y1 + y2) / 2))
-                                cv2.imwrite(path1+'/'+"anh"+str(x)+str(ix)+".jpg", anh)
-                                time.sleep(0.5)
+                            os.rename('anh/facenew', 'anh/'+x)
                         a = 1
                         break
                 cv2.imshow('webcam', im)
